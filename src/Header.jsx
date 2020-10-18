796895482e493f143b4dad8571d909486442505c
Annie Huang <annie.hn@gmail.com>
2020-10-18 18:15:06 +1100

    Use Link in Header.jsx and use NavLink so we can apply activeStyle.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
+// Link is like an anchor. But React Router handles the click so the page doesn't reload
+// NavLink support custom styling (e.g. activeStyle) when the link is active
+import {Link, NavLink} from "react-router-dom";
+
+const activeStyle = {
+  color: 'purple',
+}
+
 export default function Header() {
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            <Link to='/'>
+              <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            </Link>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to='/shoes'>Shoes</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to='/cart'>Cart</NavLink>
           </li>
         </ul>
       </nav>
