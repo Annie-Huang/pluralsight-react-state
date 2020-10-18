7f421b002569fff49293c0fdbccd17d88ae4da60
Annie Huang <annie.hn@gmail.com>
2020-10-18 17:59:48 +1100

    Get the product detail from the 2nd url parameter.

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
             <Route path='/' element={<h1>Welcome to Carved Rock Fitness</h1>} />
             {/*Display the product's category in the URL using a placeholder.*/}
             <Route path='/:category' element={<Products />} />
-            <Route path='/detail' element={<Detail />} />
+            <Route path='/:category/:id' element={<Detail />} />
             <Route path='/cart' element={<Cart />} />
           </Routes>
         </main>
