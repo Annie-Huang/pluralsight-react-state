35f4a941fa07ca26a8cc93c90ef80426b05b1e3f
Annie Huang <annie.hn@gmail.com>
2020-10-19 22:21:35 +1100

    Handle 404 error when the product is not found

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import {useParams} from 'react-router-dom';
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
   // category is getting from <Route path='/:category/:id' element={<Detail />} />
   const {id} = useParams();
   const {data: product, error, loading} = useFetch(`products/${id}`);
 
-  if (error) throw error;
   if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />; // This has to be before error. Because when 404 is throw when the product is not found, error also !== null.
+  if (error) throw error;
 
   return (
     <div id="detail">
