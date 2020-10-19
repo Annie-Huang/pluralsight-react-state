c5d7d35e941bf4431eff1925c4c8af964a6870ca
Annie Huang <annie.hn@gmail.com>
2020-10-19 21:49:56 +1100

    Mirroring Products.jsx example, in Detail.jsx. fetch data through custom useFetch hook. Display error|load|product.

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import {useParams} from 'react-router-dom';
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
 
 export default function Detail() {
-  return <h1>Detail</h1>;
+  // category is getting from <Route path='/:category/:id' element={<Detail />} />
+  const {id} = useParams();
+  const {data: product, error, loading} = useFetch(`products/${id}`);
 
-  // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  if (error) throw error;
+  if (loading) return <Spinner />;
+
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 }
